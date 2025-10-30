35699061ed09faf372dbe26fa47eb6fa9dad6c1c
Naomi Sumampow <suman910@newschool.edu>
2025-10-30 12:19:46 -0400

    Update trialsketch.js 5

--- trialsketch.js
+++ trialsketch.js
@@ -14,13 +14,19 @@ function setup() {
   amp = new p5.Amplitude();
   fft = new p5.FFT();
   
-  // Create scattered squares with same width/height but different sizes
+  // Auto-play the sound
+  sound.loop();
+  userStartAudio();
+  
+  // Create scattered squares with different sizes and speeds
   for (let i = 0; i < 5; i++) {
-    let size = random(20, 80); // random size for each square
+    let size = random(20, 80);
     rectangles.push({
       x: random(width),
       y: random(height),
-      size: size  // same width and height = square
+      size: size,
+      speedX: random(-0.5, 0.5), // slower horizontal speed
+      speedY: random(-0.5, 0.5)  // slower vertical speed
     });
   }
 }
@@ -35,24 +41,25 @@ function draw() {
   let level = amp.getLevel();
   let spectrum = fft.analyze();
   
-  // Draw squares - all same cyan color
+  // Update and draw squares
   push();
-  fill(0, 255, 255); // cyan color
+  fill(0, 255, 255);
   noStroke();
   
   for (let i = 0; i < rectangles.length; i++) {
+    // Move squares slowly
+    rectangles[i].x += rectangles[i].speedX;
+    rectangles[i].y += rectangles[i].speedY;
+    
+    // Wrap around screen edges
+    if (rectangles[i].x > width) rectangles[i].x = 0;
+    if (rectangles[i].x < 0) rectangles[i].x = width;
+    if (rectangles[i].y > height) rectangles[i].y = 0;
+    if (rectangles[i].y < 0) rectangles[i].y = height;
+    
+    // Draw square
     rect(rectangles[i].x, rectangles[i].y, rectangles[i].size, rectangles[i].size);
   }
   
   pop();
-}
-
-function mouseClicked() {
-  userStartAudio();
-  if (sound.isPlaying()) {
-    sound.pause();
-  } else {
-    sound.stop();
-    sound.loop();
-  }
 }
