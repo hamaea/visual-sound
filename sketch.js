6808915d4f902decd162151b5c03cc0d23c661fc
ellia <ellrohamabuchi@gmail.com>
2025-10-30 12:02:57 -0400

    going back older version before creating rects

--- sketch.js
+++ sketch.js
@@ -1,60 +1,47 @@
 let img;
 let sound;
-let amp;
-let fft;
-let play = false;
-let rectangles = []; 
 
-function preload(){
-  img = loadImage("assets/IMAGE1.jpg");
+let amp; //https://p5js.org/reference/p5.sound/p5.Amplitude/
+let fft; //https://p5js.org/reference/p5.sound/p5.FFT/
+
+let play = false;
+
+function preload() {
+  img = loadImage("assets/IMAGE2.jpg");
   sound = loadSound("assets/song01.mp3");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(window.innerWidth, window.innerHeight);
   amp = new p5.Amplitude();
   fft = new p5.FFT();
-  
- 
-  for (let i = 0; i < 15; i++) {
-    rectangles.push({
-      x: random(width),
-      y: random(height),
-      w: random(50, 150),
-      h: random(50, 150)
-    });
-  }
 }
 
 function draw() {
   background(220);
-  
 
-  imageMode(CORNER);
-  image(img, 0, 0, width, height);
-  
-  let level = amp.getLevel();
-  let spectrum = fft.analyze();
-  
+  imageMode(CENTER);
+  image(img, width / 2, height / 2);
+
+  let level = amp.getLevel(); // overall loudness (0 → 1);
+  let spectrum = fft.analyze(); // frequency array (0–255);
+
   console.log("Amplitude:", level);
   console.log("First frequency value:", spectrum[0]);
-  
+
   push();
   blendMode(DIFFERENCE);
   fill(255);
-  
-  for (let r of rectangles) {  
-    rect(r.x, r.y, r.w, r.h);  
-  }
-  
+  rect(width / 2, height / 2, 100, 100);
   pop();
 }
 
 function mouseClicked() {
-  userStartAudio();
+  userStartAudio(); //https://p5js.org/reference/p5/userStartAudio/
   if (sound.isPlaying()) {
-    sound.pause();
+    sound.pause(); // pause if currently playing
   } else {
+    // restart from the beginning & loop automatically
     sound.stop();
     sound.loop();
   }
