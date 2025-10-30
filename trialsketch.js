c1639e4a25ea9406ba52828797fa2bf12af96cc4
Naomi Sumampow <suman910@newschool.edu>
2025-10-30 16:41:46 -0400

    Update trialsketch.js back to image 3

--- trialsketch.js
+++ trialsketch.js
@@ -1,13 +1,13 @@
-let img;
-let sound;
-let amp;
 let fft;
 let rectangles = [];
+
+function preload(){
+  img = loadImage("assets/IMAGE1.jpg");
 let level;
 let spectrum;
 
 function preload() {
-  img = loadImage("assets/IMAGE1.jpg"); 
+  img = loadImage("assets/IMAGE2.jpg");
   sound = loadSound("assets/song01.mp3");
 }
 
@@ -16,59 +16,91 @@ function setup() {
   amp = new p5.Amplitude();
   fft = new p5.FFT();
   
+  // Auto-play the sound
+  sound.loop();
+  userStartAudio();
+  
+
   // Create more scattered squares with different sizes and faster speeds
+  for (let i = 0; i < 20; i++) {
   for (let i = 0; i < 10; i++) {
     let size = random(20, 80);
     rectangles.push({
       x: random(width),
       y: random(height),
       size: size,
+      speedX: random(-2, 15), // faster horizontal speed
+      speedY: random(-2, 15)  // faster vertical speed
       speedX: random(-3, 3), // faster horizontal speed
       speedY: random(-3, 3),  // faster vertical speed
       sensitivity: random(0.5, 2)
     });
   }
-  rectMode(CENTER);
+
 }
 
 function draw() {
   background(220);
   
+
   // Fit image to full screen
   imageMode(CORNER);
   image(img, 0, 0, width, height);
   
+  let level = amp.getLevel();
+  let spectrum = fft.analyze();
+  
+
   let level = amp.getLevel(); // overall loudness (0 → 1);
   let spectrum = fft.analyze(); // frequency array (0–255);
-  
+
+  console.log("Amplitude:", level);
+  console.log("First frequency value:", spectrum[0]);
+
   // Update and draw squares
   push();
+  fill(0, 255, 255);
+  noStroke();
+  
+  for (let i = 0; i < rectangles.length-1; i++) {
   blendMode(DIFFERENCE);
   fill(255);
+
   for (let i = 0; i < rectangles.length; i++) {
     // Move squares faster
+    rectangles[i].x += rectangles[i].speedX;
+    rectangles[i].y += rectangles[i].speedY;
+    
     rectangles[i].x += rectangles[i].speedX * (1 + level * 20);
     rectangles[i].y += rectangles[i].speedY * (1 + level * 20);
-    
+
     // Wrap around screen edges
+    if (rectangles[i].x > width) rectangles[i].x = 0;
+    if (rectangles[i].x < 0) rectangles[i].x = width;
+    if (rectangles[i].y > height) rectangles[i].y = 0;
+    if (rectangles[i].y < 0) rectangles[i].y = height;
+    
     if (rectangles[i].x > (width + 90)) rectangles[i].x = 0;
     if (rectangles[i].x < (0 - 90)) rectangles[i].x = width;
     if (rectangles[i].y > (height + 90)) rectangles[i].y = 0;
     if (rectangles[i].y < (0 - 90)) rectangles[i].y = height;
-    
+
     rectangles[i].size = map(level * rectangles[i].sensitivity, 0, 0.3, 10, 120);
-    
+
     // Draw square
     rect(rectangles[i].x, rectangles[i].y, rectangles[i].size, rectangles[i].size);
   }
+  
+
   pop();
 }
 
 function mouseClicked() {
-  userStartAudio();
+  userStartAudio(); //https://p5js.org/reference/p5/userStartAudio/
   if (sound.isPlaying()) {
-    sound.pause();
+    sound.pause(); // pause if currently playing
   } else {
+    // restart from the beginning & loop automatically
     sound.stop();
     sound.loop();
   }
