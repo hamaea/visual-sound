f8d54c4eadcc0b818d3bc9a057326fb963489dbc
ellia <ellrohamabuchi@gmail.com>
2025-10-30 11:49:41 -0400

    user start audio

--- sketch.js
+++ sketch.js
@@ -6,8 +6,7 @@ let fft; //https://p5js.org/reference/p5.sound/p5.FFT/
 
 let play = false;
 
-
-function preload(){
+function preload() {
   img = loadImage("assets/IMAGE1.jpg");
   sound = loadSound("assets/song01.mp3");
 }
@@ -20,26 +19,26 @@ function setup() {
 
 function draw() {
   background(220);
-  
-  imageMode(CENTER);
-  image(img, width/2, height/2);
 
-let level = amp.getLevel(); // overall loudness (0 → 1);
-let spectrum = fft.analyze(); // frequency array (0–255);
-  
+  imageMode(CENTER);
+  image(img, width / 2, height / 2);
+
+  let level = amp.getLevel(); // overall loudness (0 → 1);
+  let spectrum = fft.analyze(); // frequency array (0–255);
+
   console.log("Amplitude:", level);
   console.log("First frequency value:", spectrum[0]);
 
   push();
   blendMode(DIFFERENCE);
   fill(255);
-  rect(width/2, height/2, 100, 100);
+  rect(width / 2, height / 2, 100, 100);
   pop();
-  
 }
 
-function mouseClicked(){
-     if (sound.isPlaying()) {
+function mouseClicked() {
+  userStartAudio(); //https://p5js.org/reference/p5/userStartAudio/
+  if (sound.isPlaying()) {
     sound.pause(); // pause if currently playing
   } else {
     // restart from the beginning & loop automatically
